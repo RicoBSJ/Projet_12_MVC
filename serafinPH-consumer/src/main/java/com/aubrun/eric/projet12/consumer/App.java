c92d28346a37ae4b941f2830989b162e99993f6a
RicoBSJ <44947358+RicoBSJ@users.noreply.github.com>
2022-02-26 16:58:13 +0100

    FirstCommit

---
+++ serafinPH-consumer/src/main/java/com/aubrun/eric/projet12/consumer/App.java
@@ -0,0 +1,13 @@
+package com.aubrun.eric.projet12.consumer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
